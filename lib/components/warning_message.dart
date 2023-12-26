6fc03cb4829dc4f550ef0b691133ba06e3f8379f
Dian Bayu Nugroho <nugrohob825@gmail.com>
2023-12-26 23:39:39 +0700

    add button refresh component warning message

--- lib/components/warning_message.dart
+++ lib/components/warning_message.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class WarningMessage extends StatelessWidget {
   final String message;
   final String image;
+  final VoidCallback? onPressed;
+  final bool isButtonVisible;
   const WarningMessage({
     Key? key,
     required this.message,
     required this.image,
+    this.onPressed,
+    this.isButtonVisible = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.transparent,
+      color: Colors.white,
       height: double.infinity,
       width: double.infinity,
       child: Column(
@@ -28,10 +32,23 @@ class WarningMessage extends StatelessWidget {
           Text(
             message,
             style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.normal,
-            ),
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none),
           ),
+          isButtonVisible
+              ? Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: onPressed,
+                      child: const Text(
+                        "Refresh",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                )
+              : const SizedBox(),
         ],
       ),
     );
