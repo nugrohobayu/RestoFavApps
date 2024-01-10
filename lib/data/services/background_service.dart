d7773996866d4800cdf2423735c9f9140930f8ee
Dian Bayu Nugroho <nugrohob825@gmail.com>
2024-01-10 14:55:42 +0700

    add scheduling notification

---
+++ lib/data/services/background_service.dart
@@ -0,0 +1,43 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart';
+import 'package:resto_fav_apps/data/services/restaurant_service.dart';
+
+import '../../main.dart';
+import '../helpers/notification_helper.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static const String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initIsolate() {
+    IsolateNameServer.registerPortWithName(
+      port.sendPort,
+      _isolateName,
+    );
+  }
+
+  static Future<void> callback() async {
+    debugPrint('Alarm fired!');
+
+    final NotificationHelper notificationHelper = NotificationHelper();
+    // var result = await Api(Client()).fetchList();
+    var result = await RestaurantService().getList();
+    // await notificationHelper.showNotification(
+    //     flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(null);
+  }
+}
